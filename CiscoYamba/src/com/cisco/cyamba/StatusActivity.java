3e6cb498912590404e67c28827ab2cf6b394ed85
Marko Gargenta <marko@marakana.com>
2012-09-25 11:39:36 -0700

    Implemented UI Events lab with TextWatcher

--- CiscoYamba/src/com/cisco/cyamba/StatusActivity.java
+++ CiscoYamba/src/com/cisco/cyamba/StatusActivity.java
@@ -1,37 +1,67 @@
 package com.cisco.cyamba;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class StatusActivity extends Activity implements OnClickListener {
-	Button buttonUpdate;
-	EditText editStatus;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_status);
-        
-        editStatus = (EditText) findViewById(R.id.edit_status);
-        buttonUpdate = (Button) findViewById(R.id.button_update);
-        
-        buttonUpdate.setOnClickListener( this );
-    }
+public class StatusActivity extends Activity implements OnClickListener,
+		TextWatcher {
+	private static final int MAX_LENGTH = 140;
+	private Button buttonUpdate;
+	private EditText editStatus;
+	private TextView textCount;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_status, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_status);
 
+		editStatus = (EditText) findViewById(R.id.edit_status);
+		buttonUpdate = (Button) findViewById(R.id.button_update);
+		textCount = (TextView) findViewById(R.id.text_count);
+
+		textCount.setText(Integer.valueOf(MAX_LENGTH).toString());
+		buttonUpdate.setOnClickListener(this);
+		editStatus.addTextChangedListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_status, menu);
+		return true;
+	}
+
+	/** OnClickListener callback */
 	public void onClick(View v) {
 		String status = editStatus.getText().toString();
-		Log.d("Yamba", "onClicked with status: "+status);
+		Log.d("Yamba", "onClicked with status: " + status);
+	}
+
+	/** TextWatcher callbacks */
+	public void afterTextChanged(Editable s) {
+	}
+
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+	}
+
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		int size = MAX_LENGTH - editStatus.getText().length();
+		textCount.setText(Integer.valueOf(size).toString());
+		
+		if(size>25) {
+			textCount.setTextColor( Color.DKGRAY );
+		} else {
+			textCount.setTextColor( Color.RED );
+		}
 	}
 }
