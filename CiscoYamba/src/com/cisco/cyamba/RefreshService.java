d3f8bd16c84021f12dac7346015bcfd8288a0970
Marko Gargenta <marko@marakana.com>
2012-09-27 11:06:33 -0700

    Added YambaClient, updated jar

--- CiscoYamba/src/com/cisco/cyamba/RefreshService.java
+++ CiscoYamba/src/com/cisco/cyamba/RefreshService.java
@@ -2,12 +2,17 @@ package com.cisco.cyamba;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.util.Log;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
 
 public class RefreshService extends Service {
 	private static final String TAG = "RefreshService";
-	
+	private YambaClient yambaClient;
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
@@ -16,6 +21,14 @@ public class RefreshService extends Service {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+
+		// Initialize yambaClient
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		String username = prefs.getString("username", "");
+		String password = prefs.getString("password", "");
+		yambaClient = new YambaClient(username, password);
+
 		Log.d(TAG, "onCreated");
 	}
 
@@ -24,7 +37,7 @@ public class RefreshService extends Service {
 		Log.d(TAG, "onStarted");
 		return super.onStartCommand(intent, flags, startId);
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
