ff2d6a0799eb34c94a58a99799d699f4b5f3d910
Marko Gargenta <marko@marakana.com>
2012-09-26 17:18:02 -0700

    Switched to fragments for MainActivity content

--- CiscoYamba/src/com/cisco/cyamba/MainActivity.java
+++ CiscoYamba/src/com/cisco/cyamba/MainActivity.java
@@ -7,11 +7,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	StatusFragment statusFragment;
+	PrefsFragment prefsFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		// setContentView(R.layout.activity_main);
+		statusFragment = new StatusFragment();
+		prefsFragment = new PrefsFragment();
+		
+		getActionBar().setHomeButtonEnabled(true);
 	}
 
 	@Override
@@ -23,12 +29,18 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
+		case android.R.id.home:
+			getFragmentManager().beginTransaction().remove(prefsFragment)
+					.remove(statusFragment).commit();
+			return true;
 		case R.id.item_status:
-			startActivity(new Intent(this, StatusActivity.class)
-					.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT));
+			getFragmentManager().beginTransaction()
+					.replace(android.R.id.content, statusFragment).commit();
+
 			return true;
 		case R.id.item_prefs:
-			startActivity(new Intent(this, PrefsActivity.class));
+			getFragmentManager().beginTransaction()
+					.replace(android.R.id.content, prefsFragment).commit();
 			return true;
 		default:
 			return false;
