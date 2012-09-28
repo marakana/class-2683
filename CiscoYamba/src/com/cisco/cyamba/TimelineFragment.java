c75db60e8b6f3f193ad47cb5d1826b8c3dc34c1f
Marko Gargenta <marko@marakana.com>
2012-09-28 12:39:27 -0700

    Implemented Cursor Loader

--- CiscoYamba/src/com/cisco/cyamba/TimelineFragment.java
+++ CiscoYamba/src/com/cisco/cyamba/TimelineFragment.java
@@ -1,6 +1,9 @@
 package com.cisco.cyamba;
 
 import android.app.ListFragment;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.text.format.DateUtils;
@@ -9,12 +12,12 @@ import android.widget.SimpleCursorAdapter;
 import android.widget.SimpleCursorAdapter.ViewBinder;
 import android.widget.TextView;
 
-public class TimelineFragment extends ListFragment {
+public class TimelineFragment extends ListFragment implements
+		LoaderCallbacks<Cursor> {
 	private static final String[] FROM = { StatusContract.Columns.USER,
 			StatusContract.Columns.MESSAGE, StatusContract.Columns.CREATED_AT };
 	private static final int[] TO = { R.id.text_user, R.id.text_message,
 			R.id.text_created_at };
-	private Cursor cursor;
 	private SimpleCursorAdapter adapter;
 
 	private static final ViewBinder VIEW_BINDER = new ViewBinder() {
@@ -24,7 +27,7 @@ public class TimelineFragment extends ListFragment {
 				long timestamp = cursor.getLong(columnIndex);
 				CharSequence relTime = DateUtils
 						.getRelativeTimeSpanString(timestamp);
-				((TextView)view).setText(relTime);
+				((TextView) view).setText(relTime);
 				return true;
 			} else
 				return false;
@@ -36,15 +39,27 @@ public class TimelineFragment extends ListFragment {
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 
-		cursor = getActivity().getContentResolver().query(
-				StatusContract.CONTENT_URI, null, null, null,
-				StatusContract.DEFAULT_SORT_ORDER);
-
-		adapter = new SimpleCursorAdapter(getActivity(), R.layout.row, cursor,
-				FROM, TO);
+		adapter = new SimpleCursorAdapter(getActivity(), R.layout.row, null,
+				FROM, TO, 0);
 		adapter.setViewBinder(VIEW_BINDER);
-
 		setListAdapter(adapter);
+
+		getLoaderManager().initLoader(0, null, this);
+	}
+
+	// --- LoaderCallbacks ---
+
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		return new CursorLoader(getActivity(), StatusContract.CONTENT_URI,
+				null, null, null, StatusContract.DEFAULT_SORT_ORDER);
+	}
+
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		adapter.swapCursor(cursor);
+	}
+
+	public void onLoaderReset(Loader<Cursor> loader) {
+		adapter.swapCursor(null);
 	}
 
 }
